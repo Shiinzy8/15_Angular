f35142184baf51df85553a03fba17ac61251a7ec
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-26 16:03:29 +0200

    связка данных компонента и шаблона

--- Angular-coursehunters/src/app/car/car.component.ts
+++ Angular-coursehunters/src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   // selector так же можно задавать по разному
@@ -6,25 +6,25 @@ import { Component, OnInit } from '@angular/core';
   // создавать не тег app-car а любой тег с таким аттрибутом
   // selector: '.app-car' - создаем любой тег с таким классом
   selector: 'app-car',
-  // templateUrl: './car.component.html',
+  templateUrl: './car.component.html',
   // либо templateUrl or template
-  template: `
-  <h2>Single car</h2>
-  `,
+  // template: `
+  // <h2>Single car</h2>
+  // `,
   // styleUrls массив где мы перечисляем пути до файлов css
-  // styleUrls: ['./car.component.scss']
+  styleUrls: ['./car.component.scss']
   // либо styleUrl or styles
-  styles: [`
-    h2 {
-      color: fuchsia;
-    }
-  `]
+  // styles: [`
+  //   h2 {
+  //     color: fuchsia;
+  //   }
+  // `]
 })
-export class CarComponent implements OnInit {
+export class CarComponent {
+  carName = 'Ford';
+  carYear = 2001;
 
-  constructor() { }
-
-  ngOnInit() {
+  getName() {
+    return this.carName;
   }
-
 }

538ae8f0e05dbfee6830874e2b06f54911f1da55
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-27 14:36:13 +0200

    область видимости стилей

--- Angular-coursehunters/src/app/cars/cars.component.ts
+++ Angular-coursehunters/src/app/cars/cars.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.scss']
+  styleUrls: ['./cars.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CarsComponent {
 

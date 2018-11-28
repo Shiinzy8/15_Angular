1e46dbe8d93e4df459b7a5f5674c0f1c34f92d38
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-28 12:40:47 +0200

    работа с событиями

--- Angular-coursehunters/src/app/directives/background.directive.ts
+++ Angular-coursehunters/src/app/directives/background.directive.ts
@@ -1,10 +1,12 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, Host, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 // дерективы могу работать только с OnInit or OnDestroy
 export class BackgroundDirective implements OnInit {
+  @HostBinding('style.backgroundColor') background: string;
+
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
@@ -12,9 +14,38 @@ export class BackgroundDirective implements OnInit {
     // этот подход не очень правильный, у нас не всегда есть доступ к DOM дереву
     // this.element.nativeElement.style.backgroundColor = 'red';
 
-    const {nativeElement} = this.element;
+    // const {nativeElement} = this.element;
     // этот подход лучше всего
-    this.renderer.setStyle(nativeElement, 'background-color', 'blue');
-    this.renderer.addClass(nativeElement, 'white-text');
+    // this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+    // this.renderer.addClass(nativeElement, 'white-text');
+  }
+
+  // пример получения event
+  // @HostListener('mouseenter', ['$event']) mouseEnter(event: Event) {
+  //   console.log(event);
+  // }
+
+  // наведение мышки на элемент
+  // @HostListener('mouseenter') mouseEnter() {
+  //   const {nativeElement} = this.element;
+  //   this.renderer.setStyle(nativeElement, 'background-color', 'red');
+  //   this.renderer.addClass(nativeElement, 'white-text');
+  // }
+
+  // убрать мышку с элемента
+  // @HostListener('mouseleave') mouseLeave() {
+  //   const {nativeElement} = this.element;
+  //   this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
+  //   this.renderer.removeClass(nativeElement, 'white-text');
+  // }
+
+  // более короткий вариант
+  @HostListener('mouseenter') mouseEnter() {
+      this.background = 'red';
+    }
+
+  // убрать мышку с элемента
+  @HostListener('mouseleave') mouseLeave() {
+    this.background = 'transparent';
   }
 }

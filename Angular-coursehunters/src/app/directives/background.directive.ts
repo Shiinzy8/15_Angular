3871f785a16858bed8dd02e2042459d5be6a81f7
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-28 12:57:57 +0200

    передача параметров в события

--- Angular-coursehunters/src/app/directives/background.directive.ts
+++ Angular-coursehunters/src/app/directives/background.directive.ts
@@ -1,15 +1,21 @@
-import {Directive, ElementRef, Host, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, Host, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 // дерективы могу работать только с OnInit or OnDestroy
 export class BackgroundDirective implements OnInit {
+  // tslint:disable-next-line:no-inferrable-types
+  @Input() hoverColor: string = 'green';
+  // tslint:disable-next-line:no-inferrable-types
+  @Input() defaultColor: string = 'transparent';
+
   @HostBinding('style.backgroundColor') background: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
+    this.background = this.defaultColor;
     // console.log(this.element);
     // этот подход не очень правильный, у нас не всегда есть доступ к DOM дереву
     // this.element.nativeElement.style.backgroundColor = 'red';
@@ -41,11 +47,13 @@ export class BackgroundDirective implements OnInit {
 
   // более короткий вариант
   @HostListener('mouseenter') mouseEnter() {
-      this.background = 'red';
+      // this.background = 'red';
+      this.background = this.hoverColor;
     }
 
   // убрать мышку с элемента
   @HostListener('mouseleave') mouseLeave() {
-    this.background = 'transparent';
+    // this.background = 'transparent';
+    this.background = this.defaultColor;
   }
 }

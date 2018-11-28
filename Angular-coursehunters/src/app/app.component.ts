f0b8eb9aa385eba4f5b7c22a6a2443c4bd29bd9b
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-28 14:42:15 +0200

    использование pipe

--- Angular-coursehunters/src/app/app.component.ts
+++ Angular-coursehunters/src/app/app.component.ts
@@ -2,16 +2,57 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <div class="col-md-8 offset-md-2">
+    <h2>{{ name }}</h2>
+    <!-- эти pipe применим только к строковым типам данных-->
+    <h2>{{ name | lowercase }}</h2>
+    <h2>{{ name | uppercase }}</h2>
+    <h2>{{ name | slice:0:3 }}</h2>
+    <h2>{{ name | slice:0:3 | lowercase }}</h2>
+    <h2>{{ name | slice:0:3 | uppercase }}</h2>
+    <hr>
+    <!-- эти pipe применим только к числовым типам данных-->
+    <h2>{{ pi }}</h2>
+    <h2>{{ pi | number }}</h2>
+    <h2>{{ pi | number:'1.3-4' }}</h2>
+    <h2>{{ pi | number:'2.1-1' }}</h2>
+    <hr>
+    <!-- эти pipe для вывода валюты-->
+    <h2>{{ money }}</h2>
+    <h2>{{ money | currency }}</h2>
+    <h2>{{ money | currency:'EUR' }}</h2>
+    <h2>{{ money | currency:'USD':'code' }}</h2>
+    <hr>
+    <!-- эти pipe для вывода дат-->
+    <h2>{{ date }}</h2>
+    <h2>{{ date | date }}</h2>
+    <h2>{{ date | date:'fullDate' }}</h2>
+    <h2>{{ date | date:'shortDate' }}</h2>
+    <h2>{{ date | date:'shortTime' }}</h2>
+    <hr>
+    <h2>{{ amount }}</h2>
+    <h2>{{ amount | percent }}</h2>
+    <hr>
+    <h2>{{ object | json }}</h2>
+    <hr>
+    <h2><pre>{{ object | json }}</pre></h2>
+  </div>
+  `
 })
 export class AppComponent {
-  headerText = 'My own directive';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-
-  onClick(number: number) {
-    this.current = number;
-  }
+  name = 'WebForMyself';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested: {
+      xyz: 3,
+      numbers: [1, 2, 3],
+    }
+  };
 }
 
--- Angular-coursehunters/src/app/app.module.ts
+++ Angular-coursehunters/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BackgroundDirective } from './directives/background.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundDirective
   ],
   imports: [
     BrowserModule,

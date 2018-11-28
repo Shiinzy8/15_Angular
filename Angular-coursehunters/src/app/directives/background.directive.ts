d4b64d1cb32a078bb23e212eba244962debf95b3
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-28 12:18:58 +0200

    использование renderer

--- Angular-coursehunters/src/app/directives/background.directive.ts
+++ Angular-coursehunters/src/app/directives/background.directive.ts
@@ -1,13 +1,20 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 // дерективы могу работать только с OnInit or OnDestroy
 export class BackgroundDirective implements OnInit {
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
+
   ngOnInit(): void {
-    console.log(this.element);
-    this.element.nativeElement.style.backgroundColor = 'red';
+    // console.log(this.element);
+    // этот подход не очень правильный, у нас не всегда есть доступ к DOM дереву
+    // this.element.nativeElement.style.backgroundColor = 'red';
+
+    const {nativeElement} = this.element;
+    // этот подход лучше всего
+    this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+    this.renderer.addClass(nativeElement, 'white-text');
   }
-  constructor(private element: ElementRef) {}
 }
